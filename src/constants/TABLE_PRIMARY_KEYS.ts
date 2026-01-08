ac492b54374967958b562eacc4c3d0310789e7c6
Bongani Masuku <bongani@1702tech.com>
2026-01-09 00:38:33 +0200

    add database table primary keys

---
+++ src/constants/TABLE_PRIMARY_KEYS.ts
@@ -0,0 +1,9 @@
+export const RequestStateTablePrimaryKey: string = "request_state_id"
+export const RequestActionTablePrimaryKey: string = "request_action_id"
+export const RequestTablePrimaryKey: string = "request_id"
+
+module.exports = {
+  RequestStateTablePrimaryKey,
+  RequestActionTablePrimaryKey,
+  RequestTablePrimaryKey,
+}
