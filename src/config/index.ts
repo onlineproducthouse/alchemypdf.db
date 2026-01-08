3f68439ff2488adb8961e9bd515dcab06c1fafec
Bongani Masuku <bongani@1702tech.com>
2026-01-08 23:46:36 +0200

    build project config

---
+++ src/config/index.ts
@@ -0,0 +1,81 @@
+require("dotenv").config()
+
+import fs = require("fs")
+import { Options } from "sequelize"
+
+export type DatabaseConfig = {
+  dialect: string
+  username: string
+  password: string
+  host: string
+  port: string
+  database: string
+
+  sslCertName: string
+
+  dialectOptions?: {
+    ssl: {
+      ca: Buffer
+      require: boolean
+      rejectUnauthorized: boolean
+    }
+  }
+
+  options: Options
+}
+
+export type EnvironmentNames = "local" | "test" | "qa" | "prod"
+
+export type Environments = {
+  [key in EnvironmentNames]: DatabaseConfig
+}
+
+
+let DBConfig: DatabaseConfig = {
+  dialect: process.env.DB_PROTOCOL,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+
+  sslCertName: "root.cert",
+
+  options: {
+    query: { raw: true, },
+
+    define: {
+      freezeTableName: true,
+      timestamps: false,
+
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+      deletedAt: "deleted_at",
+    }
+  }
+}
+
+// if (process.env.ENVIRONMENT_NAME && process.env.ENVIRONMENT_NAME !== "local") {
+//   const certFile: string = `${require('path').resolve('./')}/${DBConfig.sslCertName}`
+//   if (fs.statSync(certFile).isFile)
+//     DBConfig = {
+//       ...DBConfig,
+//       dialectOptions: {
+//         ssl: {
+//           ca: fs.readFileSync(certFile),
+//           require: true,
+//           rejectUnauthorized: false,
+//         }
+//       }
+//     }
+// }
+
+const config: Environments = {
+  local: DBConfig,
+  test: DBConfig,
+  qa: DBConfig,
+  prod: DBConfig,
+}
+
+export default config
+module.exports = config
