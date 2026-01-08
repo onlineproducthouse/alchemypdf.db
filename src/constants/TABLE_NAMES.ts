a709250730387c1317c0eddbf7e3fd5b31786ae0
Bongani Masuku <bongani@1702tech.com>
2026-01-09 00:33:51 +0200

    add database table names

---
+++ src/constants/TABLE_NAMES.ts
@@ -0,0 +1,9 @@
+export const RequestStateTableName: string = "request_state"
+export const RequestActionTableName: string = "request_action"
+export const RequestTableName: string = "request"
+
+module.exports = {
+  RequestStateTableName,
+  RequestActionTableName,
+  RequestTableName,
+}
