40d8a3e979a7d435a1968f1c84ccb05a8ca37aa1
Bongani Masuku <bongani@1702tech.com>
2026-01-09 00:40:47 +0200

    remove request action table

--- src/constants/TABLE_NAMES.ts
+++ src/constants/TABLE_NAMES.ts
@@ -1,9 +1,7 @@
 export const RequestStateTableName: string = "request_state"
-export const RequestActionTableName: string = "request_action"
 export const RequestTableName: string = "request"
 
 module.exports = {
   RequestStateTableName,
-  RequestActionTableName,
   RequestTableName,
 }
